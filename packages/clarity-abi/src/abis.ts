915dd81488300cd4fe8ebc09c73851d14b82df2c
bestmike007 <i@bestmike007.com>
2024-01-01 02:25:08 -0600

    feat: add abi for known traits
    Signed-off-by: bestmike007 <i@bestmike007.com>

--- packages/clarity-abi/src/abis.ts
+++ packages/clarity-abi/src/abis.ts
@@ -1,3 +1,6 @@
-export { default as SIP010TraitABI } from './known-abis/sip-010-trait.js';
-export { default as OwnableTraitABI } from './known-abis/ownable-trait.js';
 export { default as Brc20TokenABI } from './known-abis/brc20-token.js';
+export { default as MultisigVoteSemiFungibleTrait } from './known-abis/multisig-vote-sft-trait.js';
+export { default as MultisigVoteTrait } from './known-abis/multisig-vote-trait.js';
+export { default as OwnableTraitABI } from './known-abis/ownable-trait.js';
+export { default as SemiFungibleTraitABI } from './known-abis/semi-fungible-trait.js';
+export { default as SIP010TraitABI } from './known-abis/sip-010-trait.js';
---
+++ packages/clarity-abi/src/known-abis/multisig-vote-trait.ts
@@ -0,0 +1,148 @@
+export default ({
+  functions: [
+    {
+      name: 'end-proposal',
+      access: 'public',
+      args: [
+        {
+          name: 'proposal-id',
+          type: 'uint128',
+        },
+      ],
+      outputs: {
+        type: {
+          response: {
+            ok: 'bool',
+            error: 'uint128',
+          },
+        },
+      },
+    },
+    {
+      name: 'propose',
+      access: 'public',
+      args: [
+        {
+          name: 'start-block-height',
+          type: 'uint128',
+        },
+        {
+          name: 'title',
+          type: {
+            'string-utf8': {
+              length: 256,
+            },
+          },
+        },
+        {
+          name: 'url',
+          type: {
+            'string-utf8': {
+              length: 256,
+            },
+          },
+        },
+        {
+          name: 'new-fee-rate-x',
+          type: 'uint128',
+        },
+        {
+          name: 'new-fee-rate-y',
+          type: 'uint128',
+        },
+      ],
+      outputs: {
+        type: {
+          response: {
+            ok: 'uint128',
+            error: 'uint128',
+          },
+        },
+      },
+    },
+    {
+      name: 'return-votes-to-member',
+      access: 'public',
+      args: [
+        {
+          name: 'token',
+          type: 'trait_reference',
+        },
+        {
+          name: 'proposal-id',
+          type: 'uint128',
+        },
+        {
+          name: 'member',
+          type: 'principal',
+        },
+      ],
+      outputs: {
+        type: {
+          response: {
+            ok: 'bool',
+            error: 'uint128',
+          },
+        },
+      },
+    },
+    {
+      name: 'vote-against',
+      access: 'public',
+      args: [
+        {
+          name: 'token',
+          type: 'trait_reference',
+        },
+        {
+          name: 'proposal-id',
+          type: 'uint128',
+        },
+        {
+          name: 'amount',
+          type: 'uint128',
+        },
+      ],
+      outputs: {
+        type: {
+          response: {
+            ok: 'uint128',
+            error: 'uint128',
+          },
+        },
+      },
+    },
+    {
+      name: 'vote-for',
+      access: 'public',
+      args: [
+        {
+          name: 'token',
+          type: 'trait_reference',
+        },
+        {
+          name: 'proposal-id',
+          type: 'uint128',
+        },
+        {
+          name: 'amount',
+          type: 'uint128',
+        },
+      ],
+      outputs: {
+        type: {
+          response: {
+            ok: 'uint128',
+            error: 'uint128',
+          },
+        },
+      },
+    },
+  ],
+  variables: [],
+  maps: [],
+  fungible_tokens: [],
+  non_fungible_tokens: [],
+  epoch: 'Epoch2_05',
+  clarity_version: 'Clarity1',
+} as const);
